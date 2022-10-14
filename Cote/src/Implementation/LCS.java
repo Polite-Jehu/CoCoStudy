31b9f9f7cdc3e59adc6f08ca4fbf8b74f97ffb61
Rogue <*****@***.com>
2022-10-14 13:45:13 +0900

    LCS
    추가

--- Cote/src/Implementation/LCS.java
+++ Cote/src/Implementation/LCS.java
@@ -8,6 +8,14 @@ public class LCS {
 		Scanner scan = new Scanner(System.in);
 		String a ="0"+scan.nextLine();
 		String b ="0"+scan.nextLine();
+		/**
+		 * LCS 각 변에 0넣고 
+		 * 걔네들은 다 0으로 채우고
+		 * 똑같으면 대각선왼쪾위 값 +1
+		 * eu? 계속 맞다는 뜻이므로
+		 * 다르면 왼쪽 or 오른쪽에서 값 +1
+		 * eu? 다른거 빼와야 하니까
+		 */
 		int[][] answer = new int[a.length()][b.length()];
 		
 		for(int i=0;i<a.length();i++)
@@ -18,9 +26,6 @@ public class LCS {
 		{
 			answer[0][j]=0;
 		}
-		
-		
-		
 		for(int i=1;i<a.length();i++)
 		{
 			for(int j=1;j<b.length();j++)
